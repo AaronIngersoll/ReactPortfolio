92d9510bfbbae4c67a560da03fda0c1e48282164
Aaron Ingersoll <flex.box.hero@gmail.com>
2023-01-04 10:25:58 -0700

    all changes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import './style/main.scss';
-
+import "./style/main.scss";
 
 function main() {
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+	const rootElement = document.querySelector(".app-wrapper");
+	const root = createRoot(rootElement);
+	root.render(
+		<Provider store={createStoreWithMiddleware(reducers)}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+		// document.querySelector(".app-wrapper")
+	);
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,28 +1,35 @@
 import React from "react";
 import download from "../../../static/assets/images/download.png";
+import { Link } from "react-router-dom";
 
 export default function () {
-  return (
-    <div className="content-page-wrapper">
-    <div className="left-column"
-      style={{
-        background: "url(" + download + ") no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center"
-      }}/>
-      <div className="right-column">
-        <ul >
-          <li>
-            School: Bottega Tech, Currently attending UVU majoring in
-            Information Systems and minoring in Applied Data Analytics
-          </li>
-          <li>
-            Work: Currently at Alder Security working as a Junior Software
-            Engineer. With a few side gigs as a Web Developer.
-          </li>
-          <li>About: A Family Man and a happy coder.</li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="content-page-wrapper">
+			<div
+				className="left-column"
+				style={{
+					background: "url(" + download + ") no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			/>
+			<div className="right-column">
+				<ul>
+					<li>
+						School: Bacheclors from UVU majoring in Information Systems and
+						minoring in Applied Data Analytics <br /> Core GPA: 3.8
+					</li>
+					<li>
+						Work Resume:{" "}
+						<a
+							href="https://docs.google.com/document/d/1gKEfdRbymmveKWMpLgy7Tj09POprnRRI/edit?usp=sharing&ouid=109367959874837075766&rtpof=true&sd=true"
+							target="_blank">
+							Resume
+						</a>
+					</li>
+					<li>About: A Family Man and a happy coder.</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
